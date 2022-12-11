464a35452d48dad183cc23c962ff2115d6881458
Owen <owen.kane54@gmail.com>
2022-12-11 13:31:41 +0000

    Connect database

---
+++ api/db.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const { Pool } = requrie("pg");
+
+const pool = new Pool({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    port: process.env.PG_PORT,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE
+});
+
+module.exports = pool;
