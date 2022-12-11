3f0de9d9c33c284ba19b1ce7613d103bd0cc7b48
Owen <owen.kane54@gmail.com>
2022-12-11 15:10:22 +0000

    Add login route

--- api/routes/auth/controller.js
+++ api/routes/auth/controller.js
@@ -1,5 +1,6 @@
 const pool = require("../../db");
 const queries = require("./queries");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
@@ -19,14 +20,67 @@ const register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         // add to database
-        const response = await pool.query(queries.register, [firstName, lastName, email, hashedPassword]);
+        const response = await pool.query(queries.register, [firstName, lastName, email, hashedPassword], (err) => {
+            if (err) {
+                return res.status(500).json({
+                    error: "Database error"
+                });
+            } else {
+                res.status(200).send({ message: "User added to database" });
+            }
+        });
 
-        res.send('New user registered');
+        const token = jwt.sign(
+            {
+                email
+            },
+            process.env.SECRET_KEY
+        );
+
     } catch (err) {
         console.error(err.message);
     }
 }
 
+const login = async (req, res) => {
+    // destructure
+    const { email, password } = req.body;
+    // check if user exists
+    const data = await pool.query(queries.doesUserExist, [email]);
+    const user = data.rows;
+
+    if (user.length === 0) {
+        return res.status(400).send('User with that email does not exist. Sign up now');
+    };
+    // check if input password matches database password
+    bcrypt.compare(password, user[0].password, (err, result) => {
+        if (err) {
+            res.status(500).json({
+                error: "Server error"
+            });
+        } else if (result === true) { // check if credentials match
+            const token = jwt.sign(
+                {
+                    email: email
+                },
+                process.env.SECRET_KEY
+            );
+            res.status(200).json({
+                message: "User signed in",
+                token: token
+            });
+        } else {
+            if (result != true) {
+                res.status(400).json({
+                    error: "Incorrect password"
+                })
+            }
+        }
+    })
+
+}
+
 module.exports = {
     register,
+    login,
 }
--- api/routes/auth/routes.js
+++ api/routes/auth/routes.js
@@ -6,5 +6,7 @@ const router = Router();
 //
 router.post('/register', controller.register);
 
+router.post('/login', controller.login);
+
 
 module.exports = router;
