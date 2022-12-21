3a083627473b49f39025e75429039fefecf13ffe
Owen <owen.kane54@gmail.com>
2022-12-21 10:03:52 +0000

    Add products component (hard-coded items)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './reset.css'
 import './App.css'
 import Header from './components/header'
+import Products from './components/products'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Products />
     </div>
   )
 }
---
+++ client/src/components/products.jsx
@@ -0,0 +1,38 @@
+export default function Products() {
+    return (
+        <div id="products">
+            <h1>Products</h1>
+
+            <div id="products-list">
+                <div className="product" >
+                    <img src="../public/watch.jpg" alt="Product Image"/>
+                    <h3>Product Name</h3>
+                    <p>Nice all black watch, very stylish...</p>
+                    <i>£150</i>
+                    <button>Add to cart</button>
+                </div>
+                <div className="product">
+                    <img src="../public/watch.jpg" alt="Product Image"/>
+                    <h3>Product Name</h3>
+                    <p>Nice all black watch, very stylish...</p>
+                    <i>£150</i>
+                    <button>Add to cart</button>
+                </div>
+                <div className="product">
+                    <img src="../public/watch.jpg" alt="Product Image"/>
+                    <h3>Product Name</h3>
+                    <p>Nice all black watch, very stylish...</p>
+                    <i>£150</i>
+                    <button>Add to cart</button>
+                </div>
+                <div className="product">
+                    <img src="../public/watch.jpg" alt="Product Image"/>
+                    <h3>Product Name</h3>
+                    <p>Nice all black watch, very stylish...</p>
+                    <i>£150</i>
+                    <button>Add to cart</button>
+                </div>
+            </div>
+        </div>
+    );
+}
