3d327b6f8283ccd5897fce4d7da31b841bb2170e
Owen <owen.kane54@gmail.com>
2023-01-11 16:35:19 +0000

    Add cost of each item and total price

--- client/src/components/cart.jsx
+++ client/src/components/cart.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import RemoveFromCart from "./removeFromCart";
 
 export default function Cart() {
 
     const [cartItems, setCartItems] = useState([]);
     const [products, setProducts] = useState([]);
+    let total = 0;
 
     const cartId = localStorage.getItem("cartId");
     
@@ -25,36 +26,46 @@ export default function Cart() {
     }, [cartId]);
 
     useEffect(() => {
-        async function test() {
-            console.log("async func working")
-            // cartItems.map(async (item) => {
-            //     console.log(item);
-            //     const product = await getProduct(item.id);
-            //     console.log(product);
-            // })
+        async function getProductsFromCartItems() {
             for (const item of cartItems) {
                 getProduct(item.productid)
             }
+            
         }
-        test();
+        getProductsFromCartItems();
     }, [cartItems]);
+
     
     return (
         <div id="cart">
             <h1>Cart</h1>
 
-            {products[0] ? products.map((item, idx) => {
-                console.log(products);
-                console.log(idx);
+            {/* loop through the products */} 
+            {products.length > 0 ? products.map((item, idx) => {
+      
+                // find that product in the cartItems array
                 let product = cartItems.find(cartItem => cartItem.productid == item.id)
-                return (
+
+
+                // remove the £ sign and turn to float
+                let price = parseFloat(item.price.replace(/[£,]+/g,""));
+
+                // set the correct price for the number of items
+                price *= product.quantity;
+                // add the price to the total
+                total += price;
+
+                return(
+                    
                     <div className="cartItem" key={idx}>
                         <p>x{product.quantity}</p>
                         <h3>{item.name}</h3>
+                        <p className="cost">£{price}</p>
                         <RemoveFromCart id={item.id} />
                     </div>
                 )
             }) : "No items in cart"}<br />
+            <h3>Total: £{total.toFixed(2)}</h3>
             <button>Checkout</button>
         </div>
     );
